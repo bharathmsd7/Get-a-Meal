0ca0acdb16492e148dd176110d58752b7bc4e19f
VHALL <42777683+bharathmsd7@users.noreply.github.com>
2023-06-10 19:45:05 +0530

    Credentials update

--- src/config/server.js
+++ src/config/server.js
@@ -1,19 +1,7 @@
-/** @format */
-
-// Dev creds
-// export const Server = {
-//   endpoint: "https://cloud.appwrite.io/v1",
-//   project: "getamealnow",
-//   collectionID: "6481d6c8df87337b0e70",
-//   databaseID: "646f6a6df2093a706b66",
-//   bucketID: "646f6cf4e14e5a80a4c5",
-// };
-
-// Test creads
 export const Server = {
   endpoint: "https://cloud.appwrite.io/v1",
-  project: "getamealnow",
-  collectionID: "64798f39a4a25dd2f146",
-  databaseID: "test-db",
-  bucketID: "test",
+  project: PORJECT_CODE,
+  collectionID: COLLECTION_ID,
+  databaseID: DATABASE_ID,
+  bucketID: STORAGE_BUCKET_ID,
 };
